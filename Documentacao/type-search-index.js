f7ea6fe9af6cd1a7a4401d54a6134be6ee25a329
NandoFollower <ermesontube@gmail.com>
2022-07-12 19:17:20 -0300

    documentacao

---
+++ Documentacao/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"main","l":"Biblioteca"},{"p":"main","l":"Copia"},{"p":"main","l":"Leitor"},{"p":"main","l":"ModeloObra"},{"p":"main","l":"Obra"},{"p":"main","l":"Teste"}];updateSearchResults();
